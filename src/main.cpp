ce5ef806927c2c0567ce6eb41c416f58325d15b8
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2022-09-14 17:07:34 -0700

    main w drive code and attempt at integrating optical (copied from old main)

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,71 @@
 #include "auton.h"
 #include "pid.h"
 #include "robot.h"
+#include "pros/colors.h"
+#include "pros/misc.h"
+#include "pros/screen.hpp"
+#include "pros/vision.h"
+#include <type_traits>
 
-
-using namespace pros;
 using namespace std;
+using namespace pros;
 
-// void resetEncoders() { //we can't add this to main.h because main.h doesn't
-// refer to robot.h (where LF, LB, etc. are located) 	LF.tare_position(); //or
-// set_zero_position(0) or set_zero_position(LF.get_position()); (sets current
-// encoder position to 0) 	LB.tare_position(); 	RF.tare_position();
-// 	RB.tare_position();
-// }
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+float indexerTarget = 0;
+float indexerDeltaPos = 0;
+int indexerDir = 0;
+int indexerScaledDeltaPos = 0;
+int voltageMacro = 0;
+
+void resetSensors() {
+	encLeft.reset();
+	encRight.reset();
+	encMid.reset();
+	con.rumble(".");
+}
+
+void printBrainScreen() {
+	screen::set_pen(COLOR_WHITE);
+	while(true) {
+		gpsData = gps1.get_status();
+		screen::erase();
+		// screen::print(TEXT_MEDIUM, 0, "encL: %d", encLeft.get_value());
+		// screen::print(TEXT_MEDIUM, 1, "encR: %d", encRight.get_value());
+		// screen::print(TEXT_MEDIUM, 2, "encM: %d", encMid.get_value());
+		screen::print(TEXT_MEDIUM, 3, "X Position: %3f", gpsData.x);
+		screen::print(TEXT_MEDIUM, 4, "Y Position: %3f", gpsData.y);
+		screen::print(TEXT_MEDIUM, 5, "Pitch: %3f", gpsData.pitch);
+		screen::print(TEXT_MEDIUM, 6, "Roll: %3f", gpsData.roll);
+		screen::print(TEXT_MEDIUM, 7, "Yaw: %3f", gpsData.yaw);
+		delay(20);
+		pros::screen::erase();
+		// pros::screen::print(TEXT_MEDIUM, 0, "encL: %d", encLeft.get_value());
+		// pros::screen::print(TEXT_MEDIUM, 1, "encR: %d", encRight.get_value());
+		// pros::screen::print(TEXT_MEDIUM, 2, "encM: %d", encMid.get_value());
+		// pros::screen::print(TEXT_MEDIUM, 0, "indexerPos: %f", INDEXER.get_position());
+		// pros::screen::print(TEXT_MEDIUM, 1, "indexerTarget: %f", indexerTarget);
+		// pros::screen::print(TEXT_MEDIUM, 2, "indexerDeltaPos: %f", indexerDeltaPos);
+		// pros::screen::print(TEXT_MEDIUM, 3, "indexerDir: %d", indexerDir);
+		// pros::screen::print(TEXT_MEDIUM, 3, "indexerScaledDeltaPos: %d", indexerScaledDeltaPos);
+		pros::delay(20);
+	}
+}
+
+void moveIndexer() { //move motor to a certain position and then back
+	while(true) {
+		INDEXER.move(indexerScaledDeltaPos);
+		pros::delay(20);
+	}
+}
+
 void on_center_button() {
-  static bool pressed = false;
-  pressed = !pressed;
-  if (pressed) {
-    pros::lcd::set_text(2, "I was pressed!");
-  } else {
-    pros::lcd::clear_line(2);
-  }
+	static bool pressed = false;
+	pressed = !pressed;
+	if (pressed) {
+		pros::lcd::set_text(2, "I was pressed!");
+	} else {
+		pros::lcd::clear_line(2);
+	}
 }
 
 /**
@@ -40,11 +79,13 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-  pros::lcd::initialize();
-  pros::lcd::set_text(1, "Hello PROS User!");
-
-  pros::lcd::register_btn1_cb(on_center_button);
-  optical.set_led_pwm(100);
+	expand.set_value(true);
+	angler.set_value(false);
+	optical.set_led_pwm(100);
+	resetSensors();
+	resetEncoders();
+	//Task Indexer_move(moveIndexer);
+	Task printValues(printBrainScreen);
 }
 
 /**
@@ -66,6 +107,18 @@ void disabled() {}
 void competition_initialize() {}
 
 /**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+
+/**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
  * the Field Management System or the VEX Competition Switch in the operator
@@ -80,293 +133,145 @@ void competition_initialize() {}
  */
 
 int max_flywheel_speed = 480;
-int flywheelVoltage = 105;
+int flywheel_voltage = 105;
 int cycle = 0;
 bool indexerToggle = false;
-// int indexerstate = -1;
-//  int indexerprevstate = -1;
-bool anglerToggle = false;
+//int indexerstate = -1;
+// int indexerprevstate = -1;
+bool anglerToggle = false; 
 bool expandToggle = false;
 bool speedToggle = false;
 bool deployExpansion = false;
 int slowSpeed = 80;
 int fastSpeed = 105;
-bool rollerOn = false;
-
 void opcontrol() {
-  int time = 0;
+	int time = 0;
 
-  while (true) {
-    // // indexerDeltaPos = indexerTarget - INDEXER.get_position();
-    // // indexerDir = int(std::abs(indexerDeltaPos)/indexerDeltaPos);
-    // // indexerScaledDeltaPos = indexerDeltaPos/1.1;
-    // // int flywheelVelocity = (FLY.get_actual_velocity() +
-    // FLY1.get_actual_velocity())/2;
-    // // double chasstempC = ((RF.get_temperature() + RB.get_temperature() +
-    // LF.get_temperature() + LB.get_temperature())/4);
-    // // double chasstempF = chasstempC *(9/5) + 32;
-    if (time % 100 == 0)
-      con.clear();
-    else if (time % 50 == 0) {
-      cycle++;
-      if (cycle % 3 == 0)
-        con.print(0, 0, "HUE: %lf", optical.get_hue());
-      if ((cycle + 1) % 3 == 0)
-        con.print(1, 0, "R: %lf", optical.get_raw().red);
-      if ((cycle + 2) % 3 == 0)
-        con.print(2, 0, "B: %lf", optical.get_raw().blue);
-    }
+	while (true) {
+		// indexerDeltaPos = indexerTarget - INDEXER.get_position();
+		// indexerDir = int(std::abs(indexerDeltaPos)/indexerDeltaPos);
+		// indexerScaledDeltaPos = indexerDeltaPos/1.1;
+		int flywheelVelocity = (FLY.get_actual_velocity() + FLY1.get_actual_velocity())/2;
+		double chasstempC = ((RF.get_temperature() + RB.get_temperature() + LF.get_temperature() + LB.get_temperature())/4);
+		double chasstempF = chasstempC *(9/5) + 32;
+		if (time % 100 == 0) con.clear();
+		else if (time % 50 == 0) {
+			cycle++;
+			if (cycle % 3 == 0) con.print(0, 0, "RPM: %d", flywheelVelocity);
+			if ((cycle+1) % 3 == 0) con.print(1, 0, "FV: %d", flywheel_voltage);
+			if ((cycle+2) % 3 == 0) con.print(2, 0, "Temp: %f", chasstempC);
+		}
+		
+		//chassis arcade drive
+		int power = con.get_analog(ANALOG_LEFT_Y); //power is defined as forward or backward
+		int RX = con.get_analog(ANALOG_RIGHT_X); //turn is defined as left (positive) or right (negative)
+		int turn = int(abs(RX) * RX / 75);
+		
+		int left = power + turn;
+		int right = power - turn;
 
-    // //chassis arcade drive
-    // int power = con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y); //power is
-    // defined as forward or backward int RX =
-    // con.get_analog(E_CONTROLLER_ANALOG_RIGHT_X); //turn is defined as left
-    // (positive) or right (negative) int turn = int(abs(RX) * RX / 75);
+		LF.move(left);
+		LB.move(left);
+		RF.move(right);
+		RB.move(right);
 
-    int left = power + turn;
-    int right = power - turn;
+		// chassis tank drive 
+		// LF.move(con.get_analog(ANALOG_LEFT_Y));
+		// LB.move(con.get_analog(ANALOG_LEFT_Y));
+		// RF.move(con.get_analog(ANALOG_RIGHT_Y));
+		// RB.move(con.get_analog(ANALOG_RIGHT_Y));
 
-    LF.move(left);
-    LB.move(left);
-    RF.move(right);
-    RB.move(right);
+		//intake
+		if (con.get_digital(E_CONTROLLER_DIGITAL_R1)){
+			INTAKE.move(127);
+		}
+		else if(con.get_digital(E_CONTROLLER_DIGITAL_R2)){
+			INTAKE.move(-127);
+		}
+		else {
+			INTAKE.move(0);
+		}
 
-    // // chassis tank drive
-    // LF.move(con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y));
-    // LB.move(con.get_analog(E_CONTROLLER_ANALOG_LEFT_Y));
-    // RF.move(con.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
-    // // RB.move(con.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y));
-    // below 20 blue above 200 red
+		//indexer
+		if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_L1)){ // using prev state to determine next direction with current state turning motor either direction + off to reduce burnout
+			indexerToggle = !indexerToggle;
+			if (indexerToggle)
+			{
+			INDEXER.move_relative(197,200);
+			}
+			if (indexerToggle==false)
+			{
+			INDEXER.move_relative(-197,200);
+			}
+		}
+		
+		//flywheel
+		if (con.get_digital(E_CONTROLLER_DIGITAL_L2)){
+			FLY.move(flywheel_voltage);
+			FLY1.move(flywheel_voltage);
+		}
+		else {
+			FLY.move(0);
+			FLY1.move(0);
+		}
+		
+		//angler
+		if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_Y)){
+			if (anglerToggle == false) {
+				angler.set_value(false);
+				anglerToggle = true;
+			} else {
+				angler.set_value(true);
+				anglerToggle = false;
+			}
+		}
 
-    // AUTONOMOUS ROUTES (need to put in autonomous method later)
-    // RED AWP
-    if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_R1)) {
+		//flywheel speed changer
+		if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_A)) {
+			con.rumble(".");
+			speedToggle = !speedToggle;
+			speedToggle == true ? flywheel_voltage = slowSpeed : flywheel_voltage = fastSpeed;
+		}
+		else if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
+			speedToggle == true ? slowSpeed-- : fastSpeed--;
+			speedToggle == true ? flywheel_voltage = slowSpeed : flywheel_voltage = fastSpeed;
+		}
+		else if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)) {
+			speedToggle == true ? slowSpeed++ : fastSpeed++;
+			speedToggle == true ? flywheel_voltage = slowSpeed : flywheel_voltage = fastSpeed;
+		}
 
-      // move forward to contact roller
-      LF.move(40);
-      LB.move(40);
-      RF.move(40);
-      RB.move(40);
-      delay(180);
+		//expansion
+		if (con.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+			deployExpansion = true;
+		}
+		if (deployExpansion == true) {
+			expand.set_value(false);
+		}
 
-      // move forward more??
-      LF.move(10);
-      LB.move(10);
-      RF.move(10);
-      RB.move(10);
+		// //blue roller
+		// if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_UP)) {
+		// 	while (optical.get_hue() > 200) { //optical reads blue, aka scored red
+        // 	INTAKE.move(-90); //we want to it to score blue
+      	// 	}
 
-      while (optical.get_hue() > 200) {
-        INTAKE.move(90);
-      }
+      	// 	if (optical.get_hue() < 20) {
+        // 	INTAKE.move(0);
+      	// 	}
+		// }
 
-      if (optical.get_hue() < 20) {
-        INTAKE.move(0);
-      }
-      // Not redundant becuase the motor does have to sotp eventually, with just
-      // the while loop it sets the voltage to 90 then never resets-
-      //-it to zero so the motor will move at 90 forever even if the while loop
-      //breaks
-      INTAKE.move(0);
+		// //red roller
+		// if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_DOWN)) {
+		// 	while (optical.get_hue() < 20) { //optical reads red, aka scored blue
+        // 	INTAKE.move(90);
+      	// 	}
 
-      // rev up flywheel
-      FLY.move(109);
-      FLY1.move(109);
+      	// 	if (optical.get_hue() > 200) {
+        // 	INTAKE.move(0);
+      	// 	}
+		// }
 
-      // backwards to shooting position
-      driveStraight(-205);
-      driveTurn(-90);
-
-      // more towards shooting position
-      driveStraight(2500);
-      driveTurn(-95.5);
-
-      // shoot disc 1
-      INDEXER.move(127); // should it be indexerToggle?
-      delay(450);
-      INDEXER.move(-127);
-      delay(700);
-
-      // shoot disc 2
-      INDEXER.move(127);
-      delay(450);
-      INDEXER.move(-127);
-      delay(600);
-
-      // intake discs
-      INTAKE.move(127);
-      driveTurn(-37);
-
-      // slow flywheel down (intermediate)
-      FLY.move(108);
-      FLY1.move(108);
-
-      // intake threestack
-      driveStraight(1000);
-	  for(int i = 0; i < 15; i++) {
-		driveStraight(50);
-	  	delay(20);
-	  }
-
-      // turn to shooting position
-      driveTurn(46);
-
-      // shoot da threestack
-      INDEXER.move(127);
-      delay(250);
-      INDEXER.move(-127);
-      delay(250);
-      INDEXER.move(127);
-      delay(250);
-      INDEXER.move(-127);
-      delay(350);
-
-      // intake more discs
-      // asdf
-    }
-
-    // RED CODE
-    //  if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_R1)){
-
-    // 	while (optical.get_hue() > 200){
-    // 		INTAKE.move(-90);
-    // 	}
-
-    // 	if (optical.get_hue() < 20){
-    // 		INTAKE.move(0);
-    // 	}
-    // }
-
-    // BLUE CODE
-    //  if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_R1)){
-
-    // 	while (optical.get_hue() < 20){
-    // 		INTAKE.move(90);
-    // 	}
-
-    // 	if (optical.get_hue() > 200){
-    // 		INTAKE.move(0);
-    // 	}
-    // }
-
-    // if(optical.get_hue() < 200){
-
-    // }
-    // else if (optical.get_hue() > 210){
-    // 	INTAKE.move(0);
-    // }
-    // else {
-    // 	INTAKE.move(0);
-    // }
-
-    // else{
-    // 	INTAKE.move(0);
-    // }
-
-    // //intake
-    // if (con.get_digital(E_CONTROLLER_DIGITAL_R1)){
-    // 	INTAKE.move(127);
-    // }
-    // else if(con.get_digital(E_CONTROLLER_DIGITAL_R2)){
-    // 	INTAKE.move(-127);
-    // }
-    // else {
-    // 	INTAKE.move(0);
-    // }
-
-    // //indexer
-    // if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_L1)){ //using prev
-    // state to determine next direction with current state turning motor either
-    // direction + off to reduce burnout 	indexerToggle = !indexerToggle; 	if
-    // (indexerToggle)
-    // 	{
-    // 	INDEXER.move_relative(197,200);
-    // 	}
-    // 	if (indexerToggle==false)
-    // 	{
-    // 	INDEXER.move_relative(-197,200);
-    // 	}
-    // }
-
-    // //flywheel
-    // if (con.get_digital(E_CONTROLLER_DIGITAL_L2)){
-    // 	FLY.move(flywheelVoltage);
-    // 	FLY1.move(flywheelVoltage);
-    // }
-    // else if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_UP)){
-    // 	FLY.move(127); //good flywheel voltage for loader: 460-480 RPM
-    // 	FLY1.move(127);
-    // }
-    // else {
-    // 	FLY.move(0);
-    // 	FLY1.move(0);
-    // }
-
-    // //angler
-    // if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_Y)){
-    // 	if (anglerToggle == false) {
-    // 		angler.set_value(false);
-    // 		anglerToggle = true;
-    // 	}
-    // 	else {
-    // 		angler.set_value(true);
-    // 		anglerToggle = false;
-    // 	}
-    // }
-
-    // //flywheel speed changer
-    // if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_A)) {
-    // 	con.rumble(".");
-    // 	speedToggle = !speedToggle;
-    // 	speedToggle == true ? flywheelVoltage = slowSpeed : flywheelVoltage =
-    // fastSpeed;
-    // }
-    // else if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_B)) {
-    // 	speedToggle == true ? slowSpeed-- : fastSpeed--;
-    // 	speedToggle == true ? flywheelVoltage = slowSpeed : flywheelVoltage =
-    // fastSpeed;
-    // }
-    // else if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_X)) {
-    // 	speedToggle == true ? slowSpeed++ : fastSpeed++;
-    // 	speedToggle == true ? flywheelVoltage = slowSpeed : flywheelVoltage =
-    // fastSpeed;
-    // }
-
-    // //reset chassis motors (what is this for???)
-    // // if (con.get_digital_new_press(E_CONTROLLER_DIGITAL_DOWN)) {
-    // // 	resetEncoders();
-    // // }
-
-    // //expansion
-    // if (con.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
-    // 	deployExpansion = true;
-    // }
-    // if (deployExpansion == true) {
-    // 	expand.set_value(false);
-    // }
-
-    // pros::delay(10);
-
-    // PID testing
-    //  if(con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
-    //  	driveStraight(1000);
-    //  }
-
-    // if(con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y)) {
-    // 	driveStraight(-3000);
-    // }
-
-    // if(con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
-    // 	driveTurn(180);
-    // }
-
-    // if(con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
-    // 	driveTurn(-90);
-    // }
-    // if(con.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
-    // {
-    // 	imu.reset();
-    // }
-
-    // time += 10;
-  }
+		time += 10;
+		pros::delay(10);
+	}
 }
