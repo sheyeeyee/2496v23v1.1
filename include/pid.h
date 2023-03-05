cb0aead27fe17f7c6fca1aa9008ab7d5b3a26fb3
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-03-04 22:34:34 -0800

    yea those bothered me too

--- include/pid.h
+++ include/pid.h
@@ -24,14 +24,14 @@ extern float viewvol;
 //tune straight constants here: setConstants(STRAIGHT_KP, STRAIGHT_KI, STRAIGHT_KD);
 #define STRAIGHT_KP 0.68 * 1 // 0.7 0.18
 #define STRAIGHT_KI 0.4 // 0.17 0.1 0
-#define STRAIGHT_KD 9 * 1// 10 8 0.4
+#define STRAIGHT_KD 9 * 1 // 10 8 0.4
 
 //tune straight integral-specific here: voltage = calcPID(target, encoderAvg, STRAIGHT_INTEGRAL_KI, STRAIGHT_MAX_INTEGRAL);
 #define STRAIGHT_INTEGRAL_KI 40
 #define STRAIGHT_MAX_INTEGRAL 14.5
 
 
-//tune turnt constants here: setConstants(TURN_KP, TURN_KI, TURN_KD);
+//tune turn constants here: setConstants(TURN_KP, TURN_KI, TURN_KD);
 #define TURN_KP 21.5 * 0.68 //12 23
 #define TURN_KI 0.34 //0,05 0.07 // 0.34
 #define TURN_KD 197 * 0.65 //80
